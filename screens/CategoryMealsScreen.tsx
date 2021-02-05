b5d2ea83cd6bcddfdc6f15d9425c9b3adda34d1c
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-05 21:07:48 +0300

    scaffolded screens

---
+++ screens/CategoryMealsScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const CategoryMealsScreen = () => {
+  return (
+    <View>
+      <Text>CategoryMealsScreen CategoryMealsScreen</Text>
+    </View>
+  );
+};
+
+export default CategoryMealsScreen;
+
+const styles = StyleSheet.create({});
---
+++ screens/MealDetailsScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const MealDetailsScreen = () => {
+  return (
+    <View>
+      <Text>MealDetailsScreen MealDetailsScreen</Text>
+    </View>
+  );
+};
+
+export default MealDetailsScreen;
+
+const styles = StyleSheet.create({});
