97ac95e08b75132b4f8f0b465fc8182344ef4d62
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-06 00:03:04 +0300

    added default navigation options

--- App.tsx
+++ App.tsx
@@ -8,19 +8,24 @@ import FavoritesScreen from "./screens/FavoritesScreen";
 import FiltersScreen from "./screens/FiltersScreen";
 import MealDetailsScreen from "./screens/MealDetailsScreen";
 
-const navigator = createStackNavigator({
-  Categories: CategoriesScreen,
-  CategoryMeals: CategoryMealsScreen,
-  Favorites: FavoritesScreen,
-  Filters: FiltersScreen,
-  MealDetails: MealDetailsScreen
-});
-
-navigator.navigationOptions = {
-  headerStyle: {
-    ...(Platform.OS === "android" && { backgroundColor: Colors.primaryColor })
+const navigator = createStackNavigator(
+  {
+    Categories: CategoriesScreen,
+    CategoryMeals: CategoryMealsScreen,
+    Favorites: FavoritesScreen,
+    Filters: FiltersScreen,
+    MealDetails: MealDetailsScreen
   },
-  headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor
-};
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        ...(Platform.OS === "android" && {
+          backgroundColor: Colors.primaryColor
+        })
+      },
+      headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor
+    }
+  }
+);
 
 export default createAppContainer(navigator);
