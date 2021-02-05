81e7f73ba200e319ae473a5126ec2760715ccabb
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-05 23:16:44 +0300

    added categories in the CategoriesScreen

--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -1,15 +1,25 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-elements";
 import { NavigationStackScreenComponent } from "react-navigation-stack";
+import { CATEGORIES } from "../data/dummy-data";
 
 const CategoriesScreen: NavigationStackScreenComponent = ({ navigation }) => {
   return (
     <View>
-      <Text>CategoriesScreen CategoriesScreen</Text>
       <Button
         title="Category Meals"
         onPress={() => navigation.navigate("CategoryMeals")}
+      />
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={item => item.id}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <View style={styles.gridItem}>
+            <Text>{item.title}</Text>
+          </View>
+        )}
       />
     </View>
   );
@@ -17,4 +27,10 @@ const CategoriesScreen: NavigationStackScreenComponent = ({ navigation }) => {
 
 export default CategoriesScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
+  }
+});
