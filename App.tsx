19166f1aec50fd23a8c4ee86b098dc82598c3aca
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-05 21:40:19 +0300

    wired up screens

--- App.tsx
+++ App.tsx
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import CategoryMealsScreen from "./screens/CategoryMealsScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
+import FiltersScreen from "./screens/FiltersScreen";
+import MealDetailsScreen from "./screens/MealDetailsScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createStackNavigator({
+  Categories: CategoriesScreen,
+  CategoryMeals: CategoryMealsScreen,
+  Favorites: FavoritesScreen,
+  Filters: FiltersScreen,
+  MealDetails: MealDetailsScreen
 });
+
+export default createAppContainer(navigator);
