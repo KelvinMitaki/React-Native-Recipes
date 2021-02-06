e58e9574f8070cc5d6d361b5757ac03bb03da040
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-07 01:08:41 +0300

    wired up sidedrawer toggle on filter screen

--- screens/FiltersScreen.tsx
+++ screens/FiltersScreen.tsx
@@ -1,13 +1,50 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import { NavigationParams, NavigationRoute } from "react-navigation";
+import { NavigationDrawerProp } from "react-navigation-drawer";
+import {
+  HeaderButton,
+  HeaderButtons,
+  Item
+} from "react-navigation-header-buttons";
+import { NavigationStackScreenComponent } from "react-navigation-stack";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "../constants/Colors";
 
-const FiltersScreen = () => {
+const FiltersScreen: NavigationStackScreenComponent = () => {
   return (
     <View>
       <Text>FiltersScreen FiltersScreen</Text>
     </View>
   );
 };
+
+FiltersScreen.navigationOptions = ({ navigation }) => ({
+  headerTitle: "Filter Meals",
+  headerLeft: () => (
+    <HeaderButtons
+      HeaderButtonComponent={props => (
+        <HeaderButton
+          {...props}
+          IconComponent={Ionicons}
+          iconSize={23}
+          color={Platform.OS === "android" ? "white" : Colors.primaryColor}
+        />
+      )}
+    >
+      <Item
+        title="Drawer"
+        iconName="ios-menu"
+        onPress={() =>
+          ((navigation as unknown) as NavigationDrawerProp<
+            NavigationRoute<NavigationParams>,
+            NavigationParams
+          >).toggleDrawer()
+        }
+      />
+    </HeaderButtons>
+  )
+});
 
 export default FiltersScreen;
 
