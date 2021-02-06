ee6cf1675f4c2ee0864a5796411e705e33375a11
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-06 22:01:47 +0300

    added favorite icon

--- screens/MealDetailsScreen.tsx
+++ screens/MealDetailsScreen.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import {
+  HeaderButton,
+  HeaderButtons,
+  Item
+} from "react-navigation-header-buttons";
 import { NavigationStackScreenComponent } from "react-navigation-stack";
+import { Ionicons } from "@expo/vector-icons";
 import { MEALS } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
 const MealDetailsScreen: NavigationStackScreenComponent<{
   mealId?: string;
@@ -16,7 +23,25 @@ const MealDetailsScreen: NavigationStackScreenComponent<{
 };
 
 MealDetailsScreen.navigationOptions = ({ navigation }) => ({
-  headerTitle: MEALS.find(m => m.id === navigation.getParam("mealId"))?.title
+  headerTitle: MEALS.find(m => m.id === navigation.getParam("mealId"))?.title,
+  headerRight: () => (
+    <HeaderButtons
+      HeaderButtonComponent={props => (
+        <HeaderButton
+          {...props}
+          IconComponent={Ionicons}
+          iconSize={23}
+          color={Platform.OS === "ios" ? Colors.primaryColor : "white"}
+        />
+      )}
+    >
+      <Item
+        title="Favorite"
+        iconName="ios-star"
+        onPress={() => console.log("mark as favorite")}
+      />
+    </HeaderButtons>
+  )
 });
 
 export default MealDetailsScreen;
