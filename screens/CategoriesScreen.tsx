4b13817240a4e78de8af34e832f47cbba1f8c781
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-06 18:36:35 +0300

    styled CategoriesScreen

--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -6,14 +6,12 @@ import {
   TouchableOpacity,
   View
 } from "react-native";
-import { Button } from "react-native-elements";
 import { NavigationStackScreenComponent } from "react-navigation-stack";
 import { CATEGORIES } from "../data/dummy-data";
 
 const CategoriesScreen: NavigationStackScreenComponent = ({ navigation }) => {
   return (
     <View>
-      <Button title="Category Meals" />
       <FlatList
         data={CATEGORIES}
         keyExtractor={item => item.id}
@@ -23,10 +21,10 @@ const CategoriesScreen: NavigationStackScreenComponent = ({ navigation }) => {
             onPress={() =>
               navigation.navigate("CategoryMeals", { id: item.id })
             }
-            style={styles.gridItem}
+            style={{ ...styles.gridItem, backgroundColor: item.color }}
           >
-            <View>
-              <Text>{item.title}</Text>
+            <View style={styles.titlePrt}>
+              <Text style={styles.title}>{item.title}</Text>
             </View>
           </TouchableOpacity>
         )}
@@ -45,6 +43,23 @@ const styles = StyleSheet.create({
   gridItem: {
     flex: 1,
     margin: 15,
-    height: 150
+    height: 150,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 10,
+    elevation: 10
+  },
+  title: {
+    paddingRight: 10,
+    paddingBottom: 10,
+    fontSize: 22,
+    fontWeight: "bold"
+  },
+  titlePrt: {
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+    flex: 1
   }
 });
